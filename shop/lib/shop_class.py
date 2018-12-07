334b890d7b8dcf7f714c0848c6aeca1654649e23
zdimon <zdimon@example.com>
2018-12-07 12:04:08 +0200

    gitpython current branch

--- shop/lib/shop_class.py
+++ shop/lib/shop_class.py
@@ -1,7 +1,10 @@
 class Shop(object):
     name = 'My Pizza shop!'
 
+from git import Repo
 
 def shop_processor(request):
- shop = Shop()           
- return {'shop': shop}
+ shop = Shop()         
+ repo = Repo('.')
+ branch_name = repo.active_branch.name  
+ return {'shop': shop, 'branch_name': branch_name}
